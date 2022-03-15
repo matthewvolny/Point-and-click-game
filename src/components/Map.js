7f6873f5856912e877182b09f7ff13d3814d3c0e
matthewvolny <matthew.volny@gmail.com>
2022-03-15 16:30:32 -0400

    added map styling, and some logic for room maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./map.css";
 
-export default function Map() {
+const roomMap = [
+  {
+    roomNum: 1,
+    mapCoordinates: [
+      { index: 1, position: "top" }, //top
+      { index: "", position: "" }, //right
+      { index: "", position: "" }, //bottom
+      { index: "", position: "" }, //left
+    ],
+  },
+];
+
+export default function Map(props) {
+  const [currentRoom, setCurrentRoom] = useState({
+    currentRoom: roomMap[0].roomNum,
+    currentRoomInfo: roomMap[0].mapCoordinates,
+  });
+
   return (
-    <div>Map</div>
-  )
+    <div className="map-container">
+      <div className="map"></div>
+    </div>
+  );
 }
