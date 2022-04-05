898428b7c6adb8bf65b64952b3585a24f2815618
matthewvolny <matthew.volny@gmail.com>
2022-04-05 09:56:14 -0400

    improved game dashboard sizing and styling

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./inventory.css";
-// import playButton from "../images/play_.png";
-// import pauseButton from "../images/pause.png";
 
 export default function Inventory(props) {
   const [inventory, setInventory] = useState([]);
@@ -25,25 +23,9 @@ export default function Inventory(props) {
     <div className="inventory-flex">
       <div className="inventory-heading">Inventory</div>
       <div className="inventory-list">{inventoryList}</div>
-      {/* <div className="player">
-        {props.isPlaying ? (
-          <img
-            onClick={() => props.toggleSong()}
-            alt="pause button"
-            src={pauseButton}
-          ></img>
-        ) : (
-          <img
-            onClick={() => props.toggleSong()}
-            alt="play button"
-            src={playButton}
-          ></img>
-        )}
-      </div> */}
       <a className="quit-game-button" href="http://localhost:3000/">
         Quit game
       </a>
-      <div className="white-spacer"></div>
     </div>
   );
 }
