4971ae16958acaa2e930fe3e3e50b6ec931480f1
matthewvolny <matthew.volny@gmail.com>
2022-04-06 13:36:32 -0400

    added soe tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import Login from "../src/components/Login";
+import Room1 from "../src/components/rooms/Room1";
 
 test("render title", () => {
   render(<App />);
@@ -7,6 +9,34 @@ test("render title", () => {
   expect(pageTitle).toBeInTheDocument();
 });
 
+test("render login input", async () => {
+  render(<Login />);
+  const formInputName = screen.getByPlaceholderText("name");
+  expect(formInputName).toBeInTheDocument();
+});
+
+//conditionally renders login text when passed a user Id
+test("pass props to Login component", () => {
+  render(<Login userId="1234" />);
+  const loginForm = screen.getByText(/Login to continue your game/i);
+  expect(loginForm).toBeInTheDocument();
+});
+
+// test("render Room1 image", () => {
+//   render(<Room1 />);
+//   const room1Image = screen.getByAltText("background");
+//   expect(room1Image).toBeInTheDocument();
+// });
+
+// test("render login form", () => {
+//   render(<Login />);
+//   const { loginForm } = render(<Login variant="default" />);
+//   expect(loginForm.firstChild).toHaveClass("login-heading");
+// });
+
+// test("user logged in", () => {
+//   render(<App />);
+// });
 //"enableLink={false} is a test setting state, then seeing if the text is found"
 // test("Does not render title", () => {
 //   render(<App enableLink={false}/>);
